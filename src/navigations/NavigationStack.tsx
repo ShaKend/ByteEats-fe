7fa5180b2a72fa7326112bd5c63a06e3c0b96f2f
ShaKend <shaunkennt@gmail.com>
2025-06-02 23:40:31 +0700

    fix: solve conflict for NavigationStack

--- src/navigations/NavigationStack.tsx
+++ src/navigations/NavigationStack.tsx
@@ -9,14 +9,15 @@ import BottomTabs from "./BottomTabs";
 import OnBoardingA from "views/OnBoarding/OnBoardingA";
 import OnBoardingB from "views/OnBoarding/OnBoardingB";
 import Verification from "views/Verification";
-import ResetPassword from "views/resetPass/ResetPassword";
 
 const Stack = createStackNavigator();
 
 export default function NavigationStack(){
     return(
         <NavigationContainer>
-            <Stack.Navigator initialRouteName="Login" screenOptions={{headerShown: false}}>
+            <Stack.Navigator initialRouteName="OnBoardingA" screenOptions={{headerShown: false}}>
+                <Stack.Screen name="OnBoardingA" component={OnBoardingA} />
+                <Stack.Screen name="OnBoardingB" component={OnBoardingB} />
                 <Stack.Screen name="Login" component={Login} />
                 <Stack.Screen name="Sign" component={Sign} />
                 <Stack.Screen name="Verification" component={Verification} />
